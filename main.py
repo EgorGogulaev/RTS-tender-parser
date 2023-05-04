e1be88d5045a39dab870a0ae871696a8fd74bbcc
Egor <gogulaevegor@gmail.com>
2023-05-04 06:47:12 +0300

    Updated database with new data

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+import os
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    os.system('git add .')
+    os.system('git commit -m "Updated database with new data"')
+    os.system('git push')
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
